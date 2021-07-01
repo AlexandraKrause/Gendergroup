8a214596aaa33f22e63cab52c1e9f39557842601
kompostieren <gombert.alina@gmail.com>
2021-07-01 09:03:44 +0200

    visuals

--- gender_project_work_together_works.R
+++ gender_project_work_together_works.R
@@ -24,6 +24,9 @@ input_table_gender <- input_table_gender %>%
          upper = as.numeric(upper))
 
 str(input_table_gender)
+
+n_years <- 40
+n_years
 ########
 
 # #Reminder about the make_variables function
@@ -46,8 +49,8 @@ str(input_table_gender)
 
 #Way 5 and 6 are still missing
 
-Way <- 2
-
+Way <- 1
+# note from Alina: only way 1 and 2 work, all others dont.
 
 decision_function <- function(x, varnames){
   
@@ -174,9 +177,12 @@ decision_function <- function(x, varnames){
     
     NPV_decision <- NPV_branch - NPV_no_branch
     
+
+  
     return(list(NPV_no_branch =  NPV_no_branch,
                 NPV_branch =  NPV_branch, 
-                NPV_decision = NPV_decision))
+                NPV_decision = NPV_decision,
+                Cashflow_decision_gender = profit_with_Own_business_branch - profit_without_Own_business_branch))
   }
   
   if(Way == 2){
@@ -249,31 +255,112 @@ mcSimulation_results_way1 <- decisionSupport::mcSimulation(
 mcSimulation_results_way2 <- decisionSupport::mcSimulation(
   estimate = decisionSupport::as.estimate(input_table_gender),
   model_function = decision_function,
-  numberOfModelRuns = 10000,
-  functionSyntax = "plainNames"
-)
-
-mcSimulation_results_way4 <- decisionSupport::mcSimulation(
-  estimate = decisionSupport::as.estimate(input_table_gender),
-  model_function = decision_function,
   numberOfModelRuns = 200,
   functionSyntax = "plainNames"
 )
 
-mcSimulation_results_way4 <- decisionSupport::mcSimulation(
-  estimate = decisionSupport::as.estimate(input_table_gender),
-  model_function = decision_function,
-  numberOfModelRuns = 200,
-  functionSyntax = "plainNames"
-)
+# mcSimulation_results_way4 <- decisionSupport::mcSimulation(
+#   estimate = decisionSupport::as.estimate(input_table_gender),
+#   model_function = decision_function,
+#   numberOfModelRuns = 200,
+#   functionSyntax = "plainNames"
+# )
+
+# mcSimulation_results_way4 <- decisionSupport::mcSimulation(
+#   estimate = decisionSupport::as.estimate(input_table_gender),
+#   model_function = decision_function,
+#   numberOfModelRuns = 200,
+#   functionSyntax = "plainNames"
+# )
 
 decisionSupport::plot_distributions(mcSimulation_object = mcSimulation_results_way2, 
                                     vars = c("NPV_no_branch", "NPV_branch"),
                                     method = 'smooth_simple_overlay', 
                                     base_size = 7)
 
+decisionSupport::plot_distributions(mcSimulation_object = mcSimulation_results_way1, 
+                                    vars = c("NPV_no_branch", "NPV_branch"),
+                                    method = 'smooth_simple_overlay', 
+                                    base_size = 7)
+
+
 
 # decisionSupport::plot_distributions(mcSimulation_object = mcSimulation_results_way4, 
 #                                     vars = c("NPV_no_branch", "NPV_branch"),
 #                                     method = 'smooth_simple_overlay', 
 #                                     base_size = 7)
+
+
+
+
+## Cashflow: Here we plot the distribution of annual cashflow 
+# over the entire simulated period for the intervention (n_years). 
+# For this we use the plot_cashflow() function 
+# which uses the specified cashflow outputs from the mcSimulation() function to show cashflow over time.
+# In the example, the cashflow_var_name is "Cashflow_decision_do".
+# In our case, it is not implemented yet in the return fuction. 
+# First, I implemented it in line 185 in the return function 
+# (Cashflow_decision_gender = profit_with_Own_business_branch - profit_without_Own_business_branch), 
+# but I think the values show, that we actually dont really have distingueshed time horizons
+# betrween pension payment and retirement. 
+# I suggest renaming all the n = 40 to n_years, which should be specified in the beginning of our script.
+# I selected the MsSimulation results of way 1 and only implemented the cashflow for way 1
+
+
+plot_cashflow(mcSimulation_object = mcSimulation_results_way1, cashflow_var_name = "Cashflow_decision_gender")
+
+
+
+## Projection to Latent Structures (PLS) analysis
+# We apply a post-hoc analysis to the mcSimulation() outputs with plsr.mcSimulation().
+# The idea is to determine the Variable Importance in the Projection (VIP) score 
+# and coefficients of a Projection to Latent Structures (PLS) regression model. 
+# This function uses the outputs of the mcSimulation(). 
+# It selects all the input variables from the decision analysis function 
+# in the parameter object and then runs a PLS regression 
+# with an outcome variable defined in the parameter resultName. 
+# In the example they use the code names(mcSimulation_results$y)[3] 
+# to select the outcome variable NPV_decision_do, 
+# which is the third element of the list y in our mcSimulation_results outputs 
+# I our case, selecting the third element of the list y in our mcSimulation_results outputs 
+# as well. it is called NPV decision.
+
+pls_result <- plsr.mcSimulation(object = mcSimulation_results_way1,
+                                resultName = names(mcSimulation_results_way1$y)[3], ncomp = 1)
+
+
+## Plot PLS: 
+# We run the plot_pls() on the results from plsr.mcSimulation() 
+# with a number of standard settings. 
+# The plot_pls() function uses the text in the label column 
+# as replacement for the default text in the variable column.
+# This will cause problems, because the label column in our input_table_gender is 
+# not helpful for labeling a plot. (it says something like financing, percent and options)
+# Potential TO Do: In our input_table_gender, we need to adjust the column label, 
+# and fill it analogue to the example input table with appropriate lables for plotting. 
+# solution: we can leave out the input_table = input_table_gender, 
+# becasue out labels column messes up. 
+
+plot_pls(pls_result, threshold = 0, )
+
+#input_table = input_table_gender Rauslassen
+
+
+# We calculate Value of Information (VoI) analysis with the Expected Value of Perfect Information (EVPI). 
+#As we learned in Lecture 8 on forecasts, EVPI measures 
+# the expected opportunity loss that is incurred when the decision-maker 
+# does not have perfect information about a particular variable. 
+# EVPI is determined by examining the influence of that variable on the output value of a decision model.
+mcSimulation_table <- data.frame(mcSimulation_results_way1$x, mcSimulation_results_way1$y[1:3])
+evpi <- multi_EVPI(mc = mcSimulation_table, first_out_var = "NPV_decision")
+plot_evpi(evpi, decision_vars = "NPV_decision")
+
+
+## in the compound figute, we are forced to use the wrong input table as an input, therefore we get bad results for some plots.
+compound_figure(mcSimulation_object = mcSimulation_results_way1, 
+                input_table = input_table_gender, plsrResults = pls_result, 
+                EVPIresults = evpi, decision_var_name = "NPV_decision", 
+                cashflow_var_name = "Cashflow_decision_gender", 
+                base_size = 7)
+
+
--- model_analysis_and_visualization.R
+++ model_analysis_and_visualization.R
@@ -1,27 +1,19 @@
 
-
-
-# Alinas Addition to the model script
-
 ## Cashflow: Here we plot the distribution of annual cashflow 
 # over the entire simulated period for the intervention (n_years). 
 # For this we use the plot_cashflow() function 
 # which uses the specified cashflow outputs from the mcSimulation() function to show cashflow over time.
 # In the example, the cashflow_var_name is "Cashflow_decision_do".
-# In our case, it could be "NPV_branch", but I don't know, if this is correct.
-# 
-
-# As the n_years is not implemented in our model, I think that the function won't work immediately.
+# In our case, it is not implemented yest in the return fuction. 
+# First, I implemented it in line 185 in the return function 
+# (Cashflow_decision_gender = profit_with_Own_business_branch - profit_without_Own_business_branch), 
+# but I think the values show, that we actually dont really have distingueshed time horizons
+# betrween pension payment and retirement. 
 # I suggest renaming all the n = 40 to n_years, which should be specified in the beginning of our script.
-# As I selected the MsSimulation results of way 1 and 
-# the script does not work,  can not run the plot_cashflow function.
+# I selected the MsSimulation results of way 1 and only implemented the cashflow for way 1
 
 
-plot_cashflow(mcSimulation_object = mcSimulation_results_way1, cashflow_var_name = "NPV_branch")
-
-
-##another option to create a cashflow
-
+plot_cashflow(mcSimulation_object = mcSimulation_results_way1, cashflow_var_name = "Cashflow_decision_gender")
 
 
 
@@ -37,10 +29,7 @@ plot_cashflow(mcSimulation_object = mcSimulation_results_way1, cashflow_var_name
 # to select the outcome variable NPV_decision_do, 
 # which is the third element of the list y in our mcSimulation_results outputs 
 # I our case, selecting the third element of the list y in our mcSimulation_results outputs 
-# will not work, because we miss the cashflow in our in the return funktion. 
-# TODO we need to add something analugue to
-# "Cashflow_decision_do = result_interv - result_n_interv)" to the return function. 
-# The plsr.mcSimulation will only work if we include the chashflow in the return function.
+# as well. it is called NPV decision.
 
 pls_result <- plsr.mcSimulation(object = mcSimulation_results_way1,
                                 resultName = names(mcSimulation_results_way1$y)[3], ncomp = 1)
@@ -53,13 +42,32 @@ pls_result <- plsr.mcSimulation(object = mcSimulation_results_way1,
 # as replacement for the default text in the variable column.
 # This will cause problems, because the label column in our input_table_gender is 
 # not helpful for labeling a plot. (it says something like financing, percent and options)
-# TO Do: In our input_table_gender, we need to adjust the column label, 
+# Potential TO Do: In our input_table_gender, we need to adjust the column label, 
 # and fill it analogue to the example input table with appropriate lables for plotting. 
+# solution: we can leave out the input_table = input_table_gender, 
+# becasue out labels column messes up. 
 
-plot_pls(pls_result, input_table_gender = input_table_gender, threshold = 0)
+plot_pls(pls_result, threshold = 0, )
 
-## EVPI analysis
-## I think this is not possible to code without a running model
+#input_table = input_table_gender Rauslassen
+
+
+# We calculate Value of Information (VoI) analysis with the Expected Value of Perfect Information (EVPI). 
+#As we learned in Lecture 8 on forecasts, EVPI measures 
+# the expected opportunity loss that is incurred when the decision-maker 
+# does not have perfect information about a particular variable. 
+# EVPI is determined by examining the influence of that variable on the output value of a decision model.
+mcSimulation_table <- data.frame(mcSimulation_results_way1$x, mcSimulation_results_way1$y[1:3])
+evpi <- multi_EVPI(mc = mcSimulation_table, first_out_var = "NPV_decision")
+plot_evpi(evpi, decision_vars = "NPV_decision")
+
+
+## in the compound figute, we are forced to use the wrong input table as an input, therefore we get bad results for some plots.
+compound_figure(mcSimulation_object = mcSimulation_results_way1, 
+                input_table = input_table_gender, plsrResults = pls_result, 
+                EVPIresults = evpi, decision_var_name = "NPV_decision", 
+                cashflow_var_name = "Cashflow_decision_gender", 
+                base_size = 7)
 
 
 # graphical options to visualize uncertainty intervals of outcomes of Monte Carlo simulations. 
@@ -69,6 +77,7 @@ plot_pls(pls_result, input_table_gender = input_table_gender, threshold = 0)
 # I do not know yet how to generate that. Maybe this works: 
 # as.data.frame.mcSimulation: Coerce Monte Carlo simulation results to a data frame.
 # https://rdrr.io/cran/decisionSupport/man/as.data.frame.mcSimulation.html
+#I do not know how to find the information for this plot. converting the simulation in data frame does not work. I do not know were to find the values.
 
 library(bayes)
 test <- data.frame("practice 1" = rnorm(n = 1000, mean = 8, sd = 1.5), 
@@ -77,4 +86,11 @@ test <- data.frame("practice 1" = rnorm(n = 1000, mean = 8, sd = 1.5),
 
 color_scheme_set("blue")
 
-mcmc_areas(test,prob = 0.9,point_est = "median")
+
+library(bayes)
+mcmc_areas(mcSimulation_results_way1,prob = 0.9,point_est = "median")
+
+data1 <- as.data.frame(mcSimulation_results_way1)
+
+library(bayesplot)
+mcmc_areas(data1,prob = 0.9,point_est = "median")
